c5c30766fca57dc30b7848271691ae6a1f6716b8
susanoxi <susanoxi.ds@gmail.com>
2023-11-19 14:02:03 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='www.kaggle.com/competitions/titanic/overview/description',
+    author='susanoxi',
+    license='MIT',
+)
